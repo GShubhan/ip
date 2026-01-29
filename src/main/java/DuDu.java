caf9c6b6408c409030f4c4c0edc34538d193e8c3
GShubhan <ShubhanGabra01@gmail.com>
2026-01-29 10:09:45 +0800

    Store tasks in array and list them

--- src/main/java/DuDu.java
+++ src/main/java/DuDu.java
@@ -7,19 +7,30 @@ public class DuDu {
         System.out.println();
         System.out.println("-------------------------------------------");
         String inData;
+        String[] list = new String[100];
+        int count =0;
         while(true) {
             Scanner scan = new Scanner(System.in);
             inData = scan.nextLine();
+            if (inData.equalsIgnoreCase("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println(count + ". " + list[i]);
+                }
+                continue;
+            }
             System.out.println("-------------------------------------------");
-            System.out.println(inData);
+            System.out.println("added: "+inData);
+            list[count]=inData;
+            count++;
             System.out.println("-------------------------------------------");
+
+
+
             if (inData.contains("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("-------------------------------------------");
                 break;
             }
         }
-
-
     }
 }
