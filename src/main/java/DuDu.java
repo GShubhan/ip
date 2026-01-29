83f390176230c1107225e4330394341c076e21dc
GShubhan <ShubhanGabra01@gmail.com>
2026-01-29 09:50:03 +0800

    Echo messages back

--- src/main/java/DuDu.java
+++ src/main/java/DuDu.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class DuDu {
     public static void main(String[] args) {
         System.out.println("-------------------------------------------");
@@ -5,8 +6,20 @@ public class DuDu {
         System.out.println("What can I do for you?");
         System.out.println();
         System.out.println("-------------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("-------------------------------------------");
+        String inData;
+        while(true) {
+            Scanner scan = new Scanner(System.in);
+            inData = scan.nextLine();
+            System.out.println("-------------------------------------------");
+            System.out.println(inData);
+            System.out.println("-------------------------------------------");
+            if (inData.contains("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("-------------------------------------------");
+                break;
+            }
+        }
+
 
     }
 }
