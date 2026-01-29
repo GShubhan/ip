4bc3a702dc95d89b2fc6c85a3bceeba8a889a3b8
GShubhan <ShubhanGabra01@gmail.com>
2026-01-30 04:34:57 +0800

    Add Mark and Unmark functionality

--- src/main/java/DuDu.java
+++ src/main/java/DuDu.java
@@ -7,32 +7,62 @@ public class DuDu {
         System.out.println();
         System.out.println("-------------------------------------------");
         String inData;
-        String[] list = new String[100];
+
+        Scanner scan = new Scanner(System.in);
+        Task[] list = new Task[100];
         int count =0;
         while(true) {
-            Scanner scan = new Scanner(System.in);
             inData = scan.nextLine();
-
+            //bye
             if (inData.equalsIgnoreCase("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("-------------------------------------------");
                 break;
             }
-
+            //list
             if (inData.equalsIgnoreCase("list")) {
+                System.out.println("-------------------------------------------");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < count; i++) {
-                    System.out.println(i+1 + ". " + list[i]);
+                    System.out.println((i + 1) + ".[" +
+                            list[i].getStatusIcon() + "] " +
+                            list[i].description);
                 }
+                System.out.println("-------------------------------------------");
                 continue;
             }
+            //mark
+            if (inData.startsWith("mark ")){
+                int index = Integer.parseInt(inData.substring(5)) - 1;
+                list[index].setDone(true);
+                System.out.println("-------------------------------------------");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [" + list[index].getStatusIcon() + "] " +
+                        list[index].description);
+                System.out.println("-------------------------------------------");
+                continue;
+            }
+
+            //unmark
+            if (inData.startsWith("unmark ")) {
+                int index = Integer.parseInt(inData.substring(7)) - 1;
+                list[index].setDone(false);
+
+                System.out.println("-------------------------------------------");
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [" + list[index].getStatusIcon() + "] " +
+                        list[index].description);
+                System.out.println("-------------------------------------------");
+                continue;
+            }
+
             System.out.println("-------------------------------------------");
-            System.out.println("added: "+inData);
-            list[count]=inData;
+            Task task = new Task (inData);
+            System.out.println("added: [" + task.getStatusIcon() + "] " + task.description);
+            list[count]=task;
             count++;
             System.out.println("-------------------------------------------");
-
-
-
         }
+        scan.close();
     }
 }
