4509c11d27cc15b03261acb192c987dcf0aadc3a
GShubhan <ShubhanGabra01@gmail.com>
2026-02-06 00:59:58 +0800

    Improve code quality

--- src/main/java/DuDu.java
+++ src/main/java/DuDu.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 
 public class DuDu {
+
+    private static final int MAX_TASKS = 100;
+    private static final String LINE = "-------------------------------------------";
+
     public static void main(String[] args) {
-        System.out.println("-------------------------------------------");
+
+        System.out.println(LINE);
         System.out.println("Hello! I'm DuDu");
         System.out.println("What can I do for you?");
-        System.out.println("-------------------------------------------");
+        System.out.println(LINE);
 
         Scanner scan = new Scanner(System.in);
-        Task[] tasks = new Task[100];
+        Task[] tasks = new Task[MAX_TASKS];
         int count = 0;
 
         while (true) {
@@ -17,18 +22,21 @@ public class DuDu {
             // bye
             if (inData.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("-------------------------------------------");
+                System.out.println(LINE);
                 break;
             }
 
             // list
             if (inData.equalsIgnoreCase("list")) {
-                System.out.println("-------------------------------------------");
+                System.out.println(LINE);
                 System.out.println("Here are the tasks in your list:");
+
                 for (int i = 0; i < count; i++) {
+                    // W4.6f + inheritance: rely on toString()
                     System.out.println((i + 1) + "." + tasks[i]);
                 }
-                System.out.println("-------------------------------------------");
+
+                System.out.println(LINE);
                 continue;
             }
 
@@ -36,10 +44,11 @@ public class DuDu {
             if (inData.startsWith("mark ")) {
                 int index = Integer.parseInt(inData.substring(5)) - 1;
                 tasks[index].setDone(true);
-                System.out.println("-------------------------------------------");
+
+                System.out.println(LINE);
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("  " + tasks[index]);
-                System.out.println("-------------------------------------------");
+                System.out.println(LINE);
                 continue;
             }
 
@@ -47,41 +56,41 @@ public class DuDu {
             if (inData.startsWith("unmark ")) {
                 int index = Integer.parseInt(inData.substring(7)) - 1;
                 tasks[index].setDone(false);
-                System.out.println("-------------------------------------------");
+
+                System.out.println(LINE);
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println("  " + tasks[index]);
-                System.out.println("-------------------------------------------");
+                System.out.println(LINE);
                 continue;
             }
 
-            // todo
+            // add todo / deadline / event
+            System.out.println(LINE);
+
             if (inData.startsWith("todo ")) {
                 tasks[count] = new Todo(inData.substring(5));
 
-                // deadline
             } else if (inData.startsWith("deadline ")) {
                 String[] parts = inData.substring(9).split(" /by ");
                 tasks[count] = new Deadline(parts[0], parts[1]);
 
-                // event
             } else if (inData.startsWith("event ")) {
                 String[] parts = inData.substring(6).split(" /from | /to ");
                 tasks[count] = new Event(parts[0], parts[1], parts[2]);
 
             } else {
                 System.out.println("Invalid command");
+                System.out.println(LINE);
                 continue;
             }
 
-            System.out.println("-------------------------------------------");
             System.out.println("Got it. I've added this task:");
             System.out.println("  " + tasks[count]);
             count++;
             System.out.println("Now you have " + count + " tasks in the list.");
-            System.out.println("-------------------------------------------");
+            System.out.println(LINE);
         }
 
         scan.close();
     }
 }
-
