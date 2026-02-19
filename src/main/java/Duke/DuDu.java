77fd525192fc2ad4a5b453c78b856a526f481a35
GShubhan <ShubhanGabra01@gmail.com>
2026-02-19 18:16:52 +0800

    Implement Level-6 Delete

--- src/main/java/Duke/DuDu.java
+++ src/main/java/Duke/DuDu.java
@@ -1,6 +1,7 @@
 package Duke;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class DuDu {
 
@@ -10,14 +11,13 @@ public class DuDu {
     public static void main(String[] args) {
 
         System.out.println(LINE);
-        System.out.println("Hello! I'm Duke.DuDu");
+        System.out.println("Hello! I'm DuDu");
         System.out.println("What can I do for you?");
         System.out.println(LINE);
 
         Scanner scan = new Scanner(System.in);
-        Task[] tasks = new Task[MAX_TASKS];
+        ArrayList<Task>  tasks = new ArrayList<Task>();
 
-        int count = 0;
 
         while (true) {
             try {
@@ -35,9 +35,8 @@ public class DuDu {
                     System.out.println(LINE);
                     System.out.println("Here are the tasks in your list:");
 
-                    for (int i = 0; i < count; i++) {
-                        // W4.6f + inheritance: rely on toString()
-                        System.out.println((i + 1) + "." + tasks[i]);
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + "." + tasks.get(i));
                     }
 
                     System.out.println(LINE);
@@ -47,13 +46,13 @@ public class DuDu {
                 // mark
                 if (inData.startsWith("mark ")) {
                     int index = Integer.parseInt(inData.substring(5)) - 1;
-                    if (index < 0 || index >= count) {
-                        throw new DuDuException("Duke.Task number does not exist.");
+                    if (index < 0 || index >= tasks.size()) {
+                        throw new DuDuException("Task number does not exist.");
                     }
-                    tasks[index].setDone(true);
+                    tasks.get(index).setDone(true);
                     System.out.println(LINE);
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("  " + tasks[index]);
+                    System.out.println("  " + tasks.get(index));
                     System.out.println(LINE);
                     continue;
                 }
@@ -61,13 +60,28 @@ public class DuDu {
                 // unmark
                 if (inData.startsWith("unmark ")) {
                     int index = Integer.parseInt(inData.substring(7)) - 1;
-                    if (index < 0 || index >= count) {
-                        throw new DuDuException("Duke.Task number does not exist.");
+                    if (index < 0 || index >= tasks.size()) {
+                        throw new DuDuException("Task number does not exist.");
                     }
-                    tasks[index].setDone(false);
+                    tasks.get(index).setDone(false);
                     System.out.println(LINE);
                     System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println("  " + tasks[index]);
+                    System.out.println("  " + tasks.get(index));
+                    System.out.println(LINE);
+                    continue;
+                }
+
+                //delete
+                if (inData.startsWith("delete ")){
+                    int index = Integer.parseInt(inData.substring(7)) - 1;
+                    if (index < 0 || index >= tasks.size()) {
+                        throw new DuDuException("Task number does not exist.");
+                    }
+                    Task removedTask = tasks.remove(index);
+                    System.out.println(LINE);
+                    System.out.println("Noted. I've removed this task:");
+                    System.out.println("  " + removedTask);
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                     System.out.println(LINE);
                     continue;
                 }
@@ -76,21 +90,21 @@ public class DuDu {
                 System.out.println(LINE);
 
                 if (inData.startsWith("todo ")) {
-                    tasks[count] = new Todo(inData.substring(5));
+                    tasks.add(tasks.size(), new Todo(inData.substring(5)));
 
                 } else if (inData.startsWith("deadline ")) {
                     String[] parts = inData.substring(9).split(" /by ");
                     if (parts.length < 2) {
-                        throw new DuDuException("Duke.Deadline must include /by.");
+                        throw new DuDuException("Deadline must include /by.");
                     }
-                    tasks[count] = new Deadline(parts[0], parts[1]);
+                    tasks.add(tasks.size(), new Deadline(parts[0], parts[1]));
 
                 } else if (inData.startsWith("event ")) {
                     String[] parts = inData.substring(6).split(" /from | /to ");
                     if (parts.length < 3) {
-                        throw new DuDuException("Duke.Event must include /from and /to.");
+                        throw new DuDuException("Event must include /from and /to.");
                     }
-                    tasks[count] = new Event(parts[0], parts[1], parts[2]);
+                    tasks.add(tasks.size(), new Event(parts[0], parts[1], parts[2]));
 
 
                 } else {
@@ -104,9 +118,8 @@ public class DuDu {
                 }
 
                 System.out.println("Got it. I've added this task:");
-                System.out.println("  " + tasks[count]);
-                count++;
-                System.out.println("Now you have " + count + " tasks in the list.");
+                System.out.println("  " + tasks.get(tasks.size() - 1));
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                 System.out.println(LINE);
             }
 
